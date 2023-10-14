5d4376fc36f56e269094aff5a5e1dc0ad189df87
diego <diegopaoliello@gmail.com>
2023-10-14 10:01:56 -0300

    Commit inicial

---
+++ l2m/src/main/java/com/l2m/l2m/L2mApplication.java
@@ -0,0 +1,13 @@
+package com.l2m.l2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2mApplication.class, args);
+	}
+
+}
---
+++ l2m/src/test/java/com/l2m/l2m/L2mApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2m.l2m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2mApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
